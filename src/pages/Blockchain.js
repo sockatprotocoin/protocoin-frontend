8a8b2f5c62d731332310da3fd9eb0ed40623e6d6
Szymon Wałachowski <szymicorp@gmail.com>
2023-01-06 13:15:06 +0100

    fix: blockchain controller horizontal flex width bug

--- src/pages/Blockchain.js
+++ src/pages/Blockchain.js
@@ -1,83 +1,76 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
 import Api from '../api/Api'
 import './Blockchain.css'
 
-class Blockchain extends Component {
-    api = new Api();
+function Blockchain() {
+    const api = new Api();
+    const [isFetching, setFetching] = useState(true)
+    const [blocks, setBlocks] = useState([])
+    const sidebarOpened = useSelector((state) => state.sidebarOpened)
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isFetching: true,
-            blocks: []
-        }
-    }
+    useEffect(() => {
+        fetchBlockchain()
+    })
 
-    componentDidMount() {
-        this.fetchBlockchain();
-    }
-
-    fetchBlockchain() {
-        this.setState({ ...this.state, isFetching: true });
-        this.api.getBlockchain()
+    function fetchBlockchain() {
+        api.getBlockchain()
             .then(response => {
-                this.setState({ blocks: response.data, isFetching: false })
+                setBlocks(response.data)
+                setFetching(false)
             })
             .catch(err => {
                 console.log(err);
-                this.setState({ ...this.state, isFetching: false });
+                setFetching(false)
             })
     }
 
-    render() {
-        if (!this.state.isFetching) {
-            return (
-            <div className="container">
-                <h1>Blockchain</h1>
-                <div className='blockchain'>
-                    {this.state.blocks.map((block, blockIndex)  =>
-                        <div key={blockIndex} className='block'>
-                            <div className='blockHash tooltip highlight'>{block.blockHash}
-                                <span className='tooltiptext'>block hash</span>
-                                </div>
-                            <div className='blockHeader highlight'>
-                                {block.header.previousBlockHash}
-                                {block.header.merkleRoot}
-                                {block.header.timestamp}
-                                {block.header.targetCompressed}
-                                {block.header.nonce}
+    return (
+        !isFetching ?
+        <div className="container" style={{maxWidth: sidebarOpened ? 'calc(100vw - 350px)' : '100%'}}>
+            <h1>Blockchain</h1>
+            <div className='blockchain'>
+                {blocks.map((block, blockIndex)  =>
+                    <div key={blockIndex} className='block'>
+                        <div className='blockHash tooltip highlight'>{block.blockHash}
+                            <span className='tooltiptext'>block hash</span>
                             </div>
-                            <div className='transactions'>
-                                {block.blockTransactions.map((transaction, transactionIndex) =>
-                                    <div key={transactionIndex} className='transaction'>
-                                        {transaction.transactionInputs.map(transactionInput =>
-                                            <div className='transactionInput highlight'>
-                                                {transactionInput.txid}
-                                                {transactionInput.vout}
-                                                {transactionInput.scriptSignature}
-                                            </div>
-                                        )}
-                                        {transaction.transactionOutputs.map((transactionOutput, outputIndex) =>
-                                            <div key={outputIndex} className='transactionOutput'>
-                                                <div className='amount tooltip highlight'>{transactionOutput.amount}
-                                                    <span className='tooltiptext'>amount: {parseInt(transactionOutput.amount, 16) / 100000000}🥫</span>
-                                                </div>
-                                                {transactionOutput.amount}
-                                                {transactionOutput.scriptSize}
-                                                {transactionOutput.script}
-                                                {transactionOutput.parent}
-                                            </div>
-                                        )}
-                                    </div>
-                                )}
-                            </div>
+                        <div className='blockHeader highlight'>
+                            {block.header.previousBlockHash}
+                            {block.header.merkleRoot}
+                            {block.header.timestamp}
+                            {block.header.targetCompressed}
+                            {block.header.nonce}
                         </div>
-                    )}
-                </div>
+                        <div className='transactions'>
+                            {block.blockTransactions.map((transaction, transactionIndex) =>
+                                <div key={transactionIndex} className='transaction'>
+                                    {transaction.transactionInputs.map(transactionInput =>
+                                        <div className='transactionInput highlight'>
+                                            {transactionInput.txid}
+                                            {transactionInput.vout}
+                                            {transactionInput.scriptSignature}
+                                        </div>
+                                    )}
+                                    {transaction.transactionOutputs.map((transactionOutput, outputIndex) =>
+                                        <div key={outputIndex} className='transactionOutput'>
+                                            <div className='amount tooltip highlight'>{transactionOutput.amount}
+                                                <span className='tooltiptext'>amount: {parseInt(transactionOutput.amount, 16) / 100000000}🥫</span>
+                                            </div>
+                                            {transactionOutput.amount}
+                                            {transactionOutput.scriptSize}
+                                            {transactionOutput.script}
+                                            {transactionOutput.parent}
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )}
             </div>
-            )
-        }
-    }
+        </div> : <></>
+    )
 }
 
 export default Blockchain
